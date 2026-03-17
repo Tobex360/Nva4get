734fc32a62e5b234439ed39b8b22e1ec5caac14a
Echefu Tobechukwu <tobex360@gmail.com>
2026-03-17 11:16:08 +0100

    bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing';
 import Navbar from './components/Navbar';
-import Register from './pages/Auth/Register';
-import Login from './pages/Auth/Login';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import Todo from './pages/Todo/Todo';
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
        <Route path='/' element={<Landing />} />
-       <Route path='register' element={<Register />} />
-       <Route path='login' element={<Login />} />
+       <Route path='/register' element={<Register />} />
+       <Route path='/login' element={<Login />} />
+       <Route path='/todo' element={<Todo />} />
       </Routes>
     </Router>
 
