e228ef4961745db6ba2a8a3ad8ef460f9967f6d7
Echefu Tobechukwu <tobex360@gmail.com>
2026-03-17 10:39:40 +0100

    server.js creation

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,30 @@ const app = express();
 const cors = require("cors");
 const mongoose = require('mongoose');
 const path = require("path");
+const authRoutes = require('./routes/authRoutes');
+
+
+
 require('dotenv').config();
+
+const PORT = process.env.PORT || 9000;
+
+const DB_URL = process.env.DB_URL;
+app.use(cors());
+app.use(express.json())
+
+app.use('/user', authRoutes);
+
+
+mongoose.connect(DB_URL).then((result)=>{
+    console.log('succesfully connected to mongodb')
+}).catch(err=>{
+    console.error('❌ MongoDB Connection Failed:');
+    console.error('Error:', err.message);
+    console.error('Full error:', err);
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at ${PORT}`);
+})
