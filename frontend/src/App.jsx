ec24284abcb609879ae1e487dbbf906fe1c4a9a2
Echefu Tobechukwu <tobex360@gmail.com>
2026-03-25 10:43:37 +0100

    fixed paths on app.jsx (Again)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing';
 import Navbar from './components/Navbar';
-import Register from './pages/auth/Register';
-import Login from './pages/auth/Login';
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login"
 import Todo from './pages/Todo/Todo';
 import Footer from './components/Footer';
 import BackToTop from './components/BackToTop';
